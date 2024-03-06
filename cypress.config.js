7d5052ba20473efed27678a46d3ea15ef0f13ba3
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-03-06 10:44:57 -0300

    Ajuste de build de pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,20 @@
 const { defineConfig } = require("cypress");
-const moment = require('moment');
-
-const dateTime = moment().format('DD-MM-YYYY_HH-mm-ss');
 
 module.exports = defineConfig({
-  e2e: {
+ e2e: {
     baseUrl: 'https://countries.petethompson.net/',
     video: true,
-  },
-  reporter: "mochawesome",
-  reporterOptions: {
-    reportDir: `cypress/reports/${dateTime}`,
-    quiet: true,
+ },
+ reporter: "mochawesome",
+ reporterOptions: {
+    reportFilename: "[datetime]-[name]-report",
+    timestamp: "longDate",
+    reportDir: `cypress/reports`,
+    quiet: false,
     overwrite: false,
     html: true,
-    json: true,
-  },
-  screenshotsFolder: `cypress/reports/${dateTime}/evidências`,
-  chromeWebSecurity: false,
-  projectId: "fz3afs",
+    json: false,
+ },
+ chromeWebSecurity: false,
+ projectId: "fz3afs",
 });
